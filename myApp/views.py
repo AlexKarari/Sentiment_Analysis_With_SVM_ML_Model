e7a4d76f9f5ee90868cafc5d5b28a1d37eca50a7
Alexander Kariuki <karari.alexander@gmail.com>
2020-09-25 02:43:44 +0800

    Sentiment Form
    Input form for user sentiment created

--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import sentimentForm
 
 # Create your views here.
 
@@ -11,4 +12,18 @@ def index(request):
     '''
     template = loader.get_template('myApp/index.html')
     return render(request, 'myApp/index.html')
+
+def get_sentiment(request):
+    '''
+    View Function to handle a user's input through a form
+    '''
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request
+        form = sentimentForm(request.POST)
+        #Check whether it's valid:
+        if form.is_valid():
+            your_sentiment = form.cleaned_data['your_sentiment']
+        else:
+            form = sentimentForm()
+        return render(request, 'myApp/index.html', {'form': form})
 
